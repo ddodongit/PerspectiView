a8c109f2d5e15f8fd45618dfbe513e8613d63537
심지연 <sim.yyeon@gmail.com>
2024-02-07 09:19:30 +0900

    Feat: add findByForeShadowing

--- back-end/src/main/java/com/example/backend/modules/story/StoryForeShadowingRepository.java
+++ back-end/src/main/java/com/example/backend/modules/story/StoryForeShadowingRepository.java
@@ -1,6 +1,10 @@
 package com.example.backend.modules.story;
 
+import com.example.backend.modules.foreshadowing.ForeShadowing;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StoryForeShadowingRepository extends JpaRepository<StoryForeShadowing, Long> {
+    List<StoryForeShadowing> findByForeShadowing(ForeShadowing foreShadowing);
 }
