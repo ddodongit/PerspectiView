e62027653e9aaade412331093e3da7597d25248c
심지연 <sim.yyeon@gmail.com>
2024-02-07 22:51:12 +0900

    Test: 복선 불러오기 테스트

--- back-end/src/test/java/com/example/backend/modules/foreshadowing/ForeShadowingServiceTest.java
+++ back-end/src/test/java/com/example/backend/modules/foreshadowing/ForeShadowingServiceTest.java
@@ -1,7 +1,11 @@
 package com.example.backend.modules.foreshadowing;
 
+import com.example.backend.modules.exception.NotFoundException;
+import com.example.backend.modules.plot.Plot;
+import com.example.backend.modules.plot.PlotRepository;
 import com.example.backend.modules.product.Product;
 import com.example.backend.modules.product.ProductRepository;
+import com.example.backend.modules.story.*;
 import com.example.backend.modules.team.Team;
 import com.example.backend.modules.team.TeamRepository;
 import com.example.backend.modules.team.TeamService;
@@ -15,6 +19,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -40,11 +45,22 @@ class ForeShadowingServiceTest {
     @Autowired
     ForeShadowingService foreShadowingService;
 
+    @Autowired
+    StoryForeShadowingRepository storyForeShadowingRepository;
 
     @Autowired
     ProductRepository productRepository;
 
     private Product product;
+
+    @Autowired
+    ContentRepository contentRepository;
+
+    @Autowired
+    PlotRepository plotRepository;
+
+    @Autowired
+    StoryRepository storyRepository;
 
     @BeforeEach
     public void setup() {
@@ -54,7 +70,35 @@ class ForeShadowingServiceTest {
                 .info("info")
                 .build();
         productRepository.save(product);
+    }
 
+    private Story storySetting(){
+
+
+        Plot plot = Plot.builder()
+                .name("plotname")
+                .color("red")
+                .product(product)
+                .build();
+        Plot p = plotRepository.save(plot);
+
+
+        Content content = Content.builder()
+                .content("Contents: StartContents")
+                .build();
+        Content c = contentRepository.save(content);
+
+        Story story = Story.builder()
+                .title("storyTitle")
+                .positionX(1)
+                .positionY(1.0)
+                .plot(p)
+                .content(c)
+                .storyForeShadowings(new HashSet<>())
+                .storyRelations(new HashSet<>())
+                .build();
+
+        return storyRepository.save(story);
     }
 
     private Team makeTeam(User user) {
@@ -88,7 +132,7 @@ class ForeShadowingServiceTest {
                 .product(product)
                 .fShadowName("fshName")
                 .fShadowContent("fshContent")
-                .fShadowClose(false)
+                .fShadowClose(null)
                 .build();
 
         User user = makeUser("nickname");
@@ -107,7 +151,7 @@ class ForeShadowingServiceTest {
                 .product(product)
                 .fShadowName("fshName")
                 .fShadowContent("fshContent")
-                .fShadowClose(false)
+                .fShadowClose(null)
                 .build();
         User user = makeUser("nickname");
         Team team = makeTeam(user);
@@ -118,11 +162,11 @@ class ForeShadowingServiceTest {
                 .product(product)
                 .fShadowName("updatedName")
                 .fShadowContent("fshContent")
-                .fShadowClose(false)
+                .fShadowClose(null)
                 .build();
 
         //when
-        ForeShadowing atferUpdate = foreShadowingService.updateForeShadowing(user, team.getId(), product.getId(),updateFS);
+        ForeShadowing atferUpdate = foreShadowingService.updateForeShadowing(user, team.getId(), product.getId(), updateFS);
 
         //then
         assertEquals(atferUpdate.getFShadowName(), updateFS.getFShadowName());
@@ -135,7 +179,7 @@ class ForeShadowingServiceTest {
                 .product(product)
                 .fShadowName("fshName")
                 .fShadowContent("fshContent")
-                .fShadowClose(false)
+                .fShadowClose(null)
                 .build();
 
         User user = makeUser("nickname");
@@ -149,24 +193,36 @@ class ForeShadowingServiceTest {
         List<ForeShadowing> foreShadowingList = foreShadowingService.findByProductId(user, team.getId(), product.getId());
         assertEquals(foreShadowingList.size(), 0);
     }
-    
+
     @Test
-    public void 복선전체조회() throws Exception{
+    public void 복선에서스토리리스트조회() throws Exception {
         //given
-        ForeShadowing foreShadowing = ForeShadowing.builder()
+        ForeShadowing setForeShadowing = ForeShadowing.builder()
                 .product(product)
                 .fShadowName("fshName")
                 .fShadowContent("fshContent")
-                .fShadowClose(false)
+                .fShadowClose(null)
                 .storyForeShadowings(null)
                 .build();
 
         User user = makeUser("nickname");
         Team team = makeTeam(user);
-        foreShadowingService.createForeShadowing(user, team.getId(), product.getId(), foreShadowing);
+        ForeShadowing fs = foreShadowingService.createForeShadowing(user, team.getId(), product.getId(), setForeShadowing);
+
+        Story story = storySetting();
+        //story 생성 후 그 중간 테이블 1개 생성
+        Story findStory = storyRepository.findById(story.getId()).orElseThrow(() -> new NotFoundException());
+        StoryForeShadowing sfs = StoryForeShadowing.builder()
+                .story(story)
+                .foreShadowing(fs)
+                .build();
+        storyForeShadowingRepository.save(sfs);
 
         //when
+        //복선으로 찾아오기
+        List<FshadowStoryIdDto> fshadowStoryIdDtos  = foreShadowingService.findStories(fs);
 
         //then
+        assertEquals(story.getId(),fshadowStoryIdDtos.get(0).getStoryId());
     }
 }
